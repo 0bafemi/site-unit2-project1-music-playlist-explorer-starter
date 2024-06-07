9392b0b66b17c79f277ad23bd8e2130733ae37e5
Femi Emmanuel Oduola <obafemi@meta.com>
2024-06-07 01:04:44 -0700

    implemented modal

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -6,6 +6,7 @@ function loadplaylists() {
         let card = playlistdata[i];
 
         let playlistCardElement = document.createElement("div");
+        playlistCardElement.className = "playlist-cards"
         playlistCardElement.innerHTML = `
                 <div>
                     <img class="card-pic" src="${card.playlist_art}">
@@ -25,6 +26,7 @@ function loadplaylists() {
         document.getElementById("cards-screen").appendChild(playlistCardElement);
 
         const like = document.getElementById(`heart-${card.playlistID}`);
+
         const likeElement = document.getElementById(`likecount-${card.playlistID}`);
 
 
@@ -50,4 +52,93 @@ function loadplaylists() {
     }
 }
 
+function loadModalOverlay(playlist){ // playlist contains the specific playlist object
+    let modalOverlay = document.getElementsByClassName('modal')[0];
+    console.log(modalOverlay);
+    modalOverlay.innerHTML = `
+    <div class="modalcontent">
+        <div class="modalhead">
+            <span class="modalpic">
+                <img class="card-pic" src="${playlist.playlist_art}">
+            </span>
+            <span class="modaltitle">
+                <div id="song-title"><h1>${playlist.playlist_name}</h1></div>
+                <div id="artiste">${playlist.playlist_creator}</div>
+            </span>
+            <span class="quit">&times;</span>
+        </div>
+    `;
+    // Time to work on the songs
+    let modalContent = document.getElementsByClassName('modalcontent')[0];
+    for (let i=0; i < playlist.songs.length; i++){
+        let song = playlist.songs[i];
+        modalContent.innerHTML += `
+        <div class="modalsong">
+            <span class="songpic">
+                <img class="song-pic" src="${song.cover_art}">
+            </span>
+            <span class="songtitle">
+                <div id="song-title"><h2>${song.title}</h2></div>
+                <div id="artiste">${song.artist}</div>
+                <div id="album">${song.album}</div>
+            </span>
+            <span class="duration">
+                <div id="artiste">${song.duration}</div>
+            </span>
+        </div>
+        `;
+    }
+    modalOverlay.innerHTML += `
+    </div>
+    `;
+    let closeButton = document.getElementsByClassName('quit')[0];
+    closeButton.addEventListener('click',  (event) => { // What does event do?
+        document.getElementsByClassName('modal')[0].style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modalOverlay){
+            document.getElementsByClassName('modal')[0].style.display = 'none';
+        }
+        });
+}
+
+
 loadplaylists();
+var cardElements = document.getElementsByClassName("playlist-cards");
+    for (let i = 0; i < cardElements.length; i ++) {
+        cardElements[i].addEventListener('click',  (event) => { // What does event do?
+            loadModalOverlay(playlistdata[i]);
+            document.getElementsByClassName('modal')[0].style.display = 'block';
+            console.log(playlistData[i]);
+        });
+    }
+
+
+{/* <div class="modalcontent">
+    <div class="modalhead">
+        <span class="modalpic">
+            <img class="card-pic" src="assets/img/playlist.png">
+        </span>
+        <span class="modaltitle">
+            <div id="song-title"><h1>Playlist Title</h1></div>
+            <div id="artiste">Creator Name</div>
+        </span>
+        <span class="quit">&times;</span>
+    </div>
+
+    <div class="modalsong">
+        <span class="songpic">
+            <img class="song-pic" src="${song.cover_art}">
+        </span>
+        <span class="songtitle">
+            <div id="song-title"><h2>${song.title}</h2></div>
+            <div id="artiste">${song.artist}</div>
+            <div id="album">${song.album}</div>
+        </span>
+        <span class="duration">
+            <div id="artiste">${song.duration}</div>
+        </span>
+    </div>
+
+</div> */}
